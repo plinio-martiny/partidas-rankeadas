e8260fa186437194786372e9c7697811ea8a22de
plinio-martiny <everton.vipcidades@gmail.com>
2023-10-07 22:36:11 -0300

    commit 1

---
+++ index.js
@@ -0,0 +1,29 @@
+function calcularNivel(vitorias, derrotas) {
+    let saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias <= 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return [saldoVitorias, nivel];
+}
+
+// Exemplo de uso da função
+let vitorias = 11;
+let derrotas = 1;
+
+let [saldoVitorias, nivel] = calcularNivel(vitorias, derrotas);
+console.log(`O Herói tem um saldo de ${saldoVitorias} pontos e está no nível de ${nivel}`);
